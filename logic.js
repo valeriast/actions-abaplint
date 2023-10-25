be93b35a09661f5820312d3cc5484f3eb3715e93
Valeria <valeria.s@outlook.com.br>
2023-10-24 21:11:21 -0300

    t

--- logic.js
+++ logic.js
@@ -18,8 +18,15 @@ function buildAnnotations() {
         end_line: issue.end.row,
         title: issue.description,
         annotation_level: "failure",
-        message: `[Issue Link](https://rules.abaplint.org/${issueKey})`,
-        raw_details: `https://rules.abaplint.org/${issue.key}`
+        message: issueKey,
+        raw_details: `https://rules.abaplint.org/${issue.key}`,
+        actions: [
+          {
+            label: 'Dismiss',
+            description: 'Dismiss this issue',
+            identifier: 'dismiss_button'
+          }
+        ]
       });
 
     if (annotations.length === 500) {
