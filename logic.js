7b1c72fdd98906d03c90784c21f082d06fa11eee
Valeria <valeria.s@outlook.com.br>
2023-10-24 21:59:03 -0300

    added rule link

--- logic.js
+++ logic.js
@@ -18,7 +18,7 @@ function buildAnnotations() {
         end_line: issue.end.row,
         title: issue.description,
         annotation_level: "failure",
-        message: `Rule: ${issue.key} | ` + `https://rules.abaplint.org/${issue.key}`
+        message: issue.key + ' ' + `https://rules.abaplint.org/${issue.key}`
       });
 
     if (annotations.length === 500) {
