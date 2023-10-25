d1ffc920c6c79d09a3dce30c94e771c545aeb38d
Valeria <valeria.s@outlook.com.br>
2023-10-24 22:00:20 -0300

    teste

--- logic.js
+++ logic.js
@@ -18,7 +18,7 @@ function buildAnnotations() {
         end_line: issue.end.row,
         title: issue.description,
         annotation_level: "failure",
-        message: issue.key + ' ' + `https://rules.abaplint.org/${issue.key}`
+        message: `https://rules.abaplint.org/${issue.key}`
       });
 
     if (annotations.length === 500) {
